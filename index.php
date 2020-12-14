e0e3f7edeb1e89e6f6defe89f76c44e1393cc496
adam-fraga <adam.fraga@laplateforme.io>
2020-12-14 12:37:25 +0100

    Creation fichiers projet debut header

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Swap</title>
+</head>
+<body>
+<!--HEADER SITE-->
+<header class="header">
+<!--    MAIN TITRE-->
+    <h1 class="main-title">Bienvenue chez SWAP!</h1>
+<!--    NAVIGATION BAR-->
+    <nav class="nav">
+<!--        MENU -->
+        <ul class="menu">
+            <li class="item-menu"><a href="#">User</a></li>
+<!--            SOUS MENU UTILISATEUR-->
+            <ul class="sous-menu">
+                <li class="item-sous-menu"><a href="#">Inscription</a></li>
+                <li class="item-sous-menu"><a href="#">Connexion</a></li>
+                <li class="item-sous-menu"><a href="#">Mon Profil</a></li>
+            </ul>
+            <li class="item-menu"><a href="#">Réservation</a></li>
+<!--            SOUS MENU RESERVATION-->
+            <ul class="sous-menu">
+                <li class="item-sous-menu"><a href="">Planing des réservations</a></li>
+                <li class="item-sous-menu"><a href="">Réserver une salle</a></li>
+            </ul>
+        </ul>
+    </nav>
+</header>
+
+<footer class="footer">
+
+</footer>
+</body>
+</html>
