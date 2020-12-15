71233b042d4bf015dd253c025aef8a23821b669e
adam-fraga <adam.fraga@laplateforme.io>
2020-12-15 16:25:04 +0100

    Struct site index header footer v1

--- index.php
+++ index.php
@@ -5,36 +5,99 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Swap</title>
+    <link rel="stylesheet" href="style/index.css">
+    <link rel="stylesheet" href="style/global.css">
+    <title>Life-Path</title>
 </head>
 <body>
 <!--HEADER SITE-->
 <header class="header">
-<!--    MAIN TITRE-->
-    <h1 class="main-title">Bienvenue chez SWAP!</h1>
-<!--    NAVIGATION BAR-->
+    <!--    NAVIGATION BAR-->
     <nav class="nav">
-<!--        MENU -->
-        <ul class="menu">
-            <li class="item-menu"><a href="#">User</a></li>
-<!--            SOUS MENU UTILISATEUR-->
-            <ul class="sous-menu">
-                <li class="item-sous-menu"><a href="#">Inscription</a></li>
-                <li class="item-sous-menu"><a href="#">Connexion</a></li>
-                <li class="item-sous-menu"><a href="#">Mon Profil</a></li>
-            </ul>
-            <li class="item-menu"><a href="#">Réservation</a></li>
-<!--            SOUS MENU RESERVATION-->
-            <ul class="sous-menu">
-                <li class="item-sous-menu"><a href="">Planing des réservations</a></li>
-                <li class="item-sous-menu"><a href="">Réserver une salle</a></li>
-            </ul>
+        <!--        MENU -->
+        <ul class="menu container-row">
+            <li class="item-menu link-user"><a href="#">User</a>
+                <!--            SOUS MENU UTILISATEUR-->
+                <ul class="sous-menu sm-user">
+                    <li class="item-sous-menu"><a href="#">Inscription</a></li>
+                    <li class="item-sous-menu"><a href="#">Connexion</a></li>
+                    <li class="item-sous-menu"><a href="#">Mon Profil</a></li>
+                </ul>
+            </li>
+            <li class="item-menu link-reservation"><a href="#">Réservation</a>
+                <!--            SOUS MENU RESERVATION-->
+                <ul class="sous-menu sm-reservation">
+                    <li class="item-sous-menu"><a href="">Planing</a></li>
+                    <li class="item-sous-menu"><a href="">Réserver</a></li>
+                </ul>
+            </li>
+            <form action="" method="post" class="container-tools">
+                <button class="button button-logout">Déconnexion</button>
+            </form>
         </ul>
+
     </nav>
 </header>
-
-<footer class="footer">
-
+<!--Main content-->
+<!--TITRE SITE-->
+<h1 class="main-title">Lifepath</h1>
+<main class="main">
+    <!--    BANNIERE-->
+    <div class="container-large banniere">Bloc bannière</div>
+    <!--    Section presentation du site-->
+    <section class="presentation container-col">
+        <h2 class="title">Titre section présentation</h2>
+        <p> Article de presentation Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Aliquid consequuntur iure nemo nobis quasi reprehenderit unde velit? Alias cupiditate
+            doloribus, error expedita fugit minus odio odit quis unde veniam. Ipsam Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Cupiditate deserunt facere fuga, illo inventore ipsam ipsum molestias
+            praesentium quam quas similique unde ut! Cupiditate debitis incidunt iure officiis rerum ut! Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit. Cumque dolore exercitationem, illo modi quod tempora vitae
+            voluptas! Accusamus ad aperiam obcaecati, pariatur quod ullam. Accusamus magnam modi quam repellat sed..
+        </p>
+        <h2 class="title">Les Formations</h2>
+    </section>
+    <!--    Presentation de la formation-->
+    <section class="presentation-formation container-col">
+        <div class="container-card">
+            <!--            CARD-->
+            <div class="card ">
+                <h3 class="title">Titre card1</h3>
+                <article class="card-content">
+                    Contenue card 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate doloremque eius
+                    illo impedit iste magni modi natus nisi perferendis rem sint, sunt vitae! Aut cupiditate hic
+                    officia, possimus quae voluptates!
+                </article>
+            </div>
+            <div class="card ">
+                <h3 class="title">Titre card2</h3>
+                <article class="card-content">
+                    Contenue card 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate doloremque eius
+                    illo impedit iste magni modi natus nisi perferendis rem sint, sunt vitae! Aut cupiditate hic
+                    officia, possimus quae voluptates!
+                </article>
+            </div>
+            <div class="card ">
+                <h3 class="title">Titre card3</h3>
+                <article class="card-content">
+                    Contenue card 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate doloremque eius
+                    illo impedit iste magni modi natus nisi perferendis rem sint, sunt vitae! Aut cupiditate hic
+                    officia, possimus quae voluptates!
+                </article>
+            </div>
+        </div>
+    </section>
+</main>
+<!--FOOTER-->
+<footer class="footer container-col">
+    <nav class="foot-nav">
+        <ul class="footer-menu container-row">
+            <li><a href="#">Me suivre sur les réseaux</a></li>
+            <li><a href="#">Liens externes</a></li>
+            <li><a href="#">Me contacter</a></li>
+        </ul>
+    </nav>
+    <aside class="copyright"> <small>© Adam FRAGA</small> </aside>
 </footer>
 </body>
 </html>
