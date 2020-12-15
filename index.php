7cdb65c57e7e0eedab7642c9d2268de1941253ec
adam-fraga <adam.fraga@laplateforme.io>
2020-12-15 20:19:49 +0100

    Struct ok manque MEF + Back

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style/index.css">
     <link rel="stylesheet" href="style/global.css">
+    <script src="https://kit.fontawesome.com/a95f1c7873.js" crossorigin="anonymous"></script>
     <title>Life-Path</title>
 </head>
 <body>
@@ -16,19 +17,22 @@
     <nav class="nav">
         <!--        MENU -->
         <ul class="menu container-row">
-            <li class="item-menu link-user"><a href="#">User</a>
+            <li class="item-menu link-user"><a href="#"><i class="fas fa-user">User</i></a>
                 <!--            SOUS MENU UTILISATEUR-->
                 <ul class="sous-menu sm-user">
-                    <li class="item-sous-menu"><a href="#">Inscription</a></li>
-                    <li class="item-sous-menu"><a href="#">Connexion</a></li>
-                    <li class="item-sous-menu"><a href="#">Mon Profil</a></li>
+                    <li class="item-sous-menu"><a href="site/inscription.php">Inscription</a></li>
+                    <li class="item-sous-menu"><a href="site/connexion.php">Connexion</a></li>
+                    <li class="item-sous-menu"><a href="site/profil.php">Mon Profil</a></li>
                 </ul>
             </li>
-            <li class="item-menu link-reservation"><a href="#">Réservation</a>
+            <li class="item-menu link-reservation"><a href="#"><i class="far fa-calendar-plus">Réservation</i></a>
                 <!--            SOUS MENU RESERVATION-->
                 <ul class="sous-menu sm-reservation">
-                    <li class="item-sous-menu"><a href="">Planing</a></li>
-                    <li class="item-sous-menu"><a href="">Réserver</a></li>
+                    <li class="item-sous-menu"><a href="site/planing.php">Planing</a></li>
+                    <li class="item-sous-menu"><a href="site/reservation-form.php">Réserver</a></li>
+                    <li class="item-sous-menu"><a href="site/reservation.php">Mes réservations</a></li>
+
+
                 </ul>
             </li>
             <form action="" method="post" class="container-tools">
@@ -40,29 +44,27 @@
 </header>
 <!--Main content-->
 <!--TITRE SITE-->
-<h1 class="main-title">Lifepath</h1>
+<h1 class="main-title">CODING SCHOOL</h1>
 <main class="main">
     <!--    BANNIERE-->
-    <div class="container-large banniere">Bloc bannière</div>
+    <div class="container-large banniere"></div>
     <!--    Section presentation du site-->
     <section class="presentation container-col">
-        <h2 class="title">Titre section présentation</h2>
-        <p> Article de presentation Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit. Aliquid consequuntur iure nemo nobis quasi reprehenderit unde velit? Alias cupiditate
-            doloribus, error expedita fugit minus odio odit quis unde veniam. Ipsam Lorem ipsum dolor sit amet,
-            consectetur adipisicing elit. Cupiditate deserunt facere fuga, illo inventore ipsam ipsum molestias
-            praesentium quam quas similique unde ut! Cupiditate debitis incidunt iure officiis rerum ut! Lorem ipsum
-            dolor sit amet, consectetur adipisicing elit. Cumque dolore exercitationem, illo modi quod tempora vitae
-            voluptas! Accusamus ad aperiam obcaecati, pariatur quod ullam. Accusamus magnam modi quam repellat sed..
+        <h2 class="title">Lorem</h2>
+        <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, iste mollitia numquam officia quaerat
+            voluptas! Alias aperiam at beatae dolor doloribus ducimus ea et ex, hic illo minus sit tempora. Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit. Accusantium amet aut autem dolore doloremque ea eius harum,
+            impedit incidunt iure labore magni maiores neque nostrum nulla obcaecati odit quaerat tempora.
         </p>
-        <h2 class="title">Les Formations</h2>
+
     </section>
     <!--    Presentation de la formation-->
     <section class="presentation-formation container-col">
+        <h2 class="title">Les différentes salles</h2>
         <div class="container-card">
             <!--            CARD-->
             <div class="card ">
-                <h3 class="title">Titre card1</h3>
+                <h3 class="title">Salle de Coworking</h3>
                 <article class="card-content">
                     Contenue card 1 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate doloremque eius
                     illo impedit iste magni modi natus nisi perferendis rem sint, sunt vitae! Aut cupiditate hic
@@ -70,7 +72,7 @@
                 </article>
             </div>
             <div class="card ">
-                <h3 class="title">Titre card2</h3>
+                <h3 class="title">Black Room</h3>
                 <article class="card-content">
                     Contenue card 2 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate doloremque eius
                     illo impedit iste magni modi natus nisi perferendis rem sint, sunt vitae! Aut cupiditate hic
@@ -78,7 +80,7 @@
                 </article>
             </div>
             <div class="card ">
-                <h3 class="title">Titre card3</h3>
+                <h3 class="title">Le Bordel</h3>
                 <article class="card-content">
                     Contenue card 3 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate doloremque eius
                     illo impedit iste magni modi natus nisi perferendis rem sint, sunt vitae! Aut cupiditate hic
@@ -97,7 +99,7 @@
             <li><a href="#">Me contacter</a></li>
         </ul>
     </nav>
-    <aside class="copyright"> <small>© Adam FRAGA</small> </aside>
+    <aside class="copyright"><small>© Adam FRAGA</small></aside>
 </footer>
 </body>
 </html>
